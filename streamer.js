77c781d554a1abc3f87cee9d30bcdf27ce61f957
Ernie3 <ernestgwilsoniii@gmail.com>
2019-01-22 06:47:07 -0500

    use ffmpeg

--- streamer.js
+++ streamer.js
@@ -6,43 +6,43 @@ const NALseparator    = new Buffer([0,0,0,1]);//NAL break
 function start(socket){
 console.log("==>>>>>start streaming")
 
-// var proc=spawn("ffmpeg",[
-// 						"-s","640x360",
-// 						"-re",
-// 						"-framerate","30",
-// 						"-pixel_format","yuv420p",//"yuv420p",//yuyv422 
-// 						//"-f","rawvideo",
-// 						"-i","/dev/video0",
-// 						// "-c:v","h264_mmal",
-// 						// "-i","/home/pi/360.mp4",
-// 						"-c:v","h264_omx",
-// 						"-b:v","1M",
-// 						"-s","640x360",
-// 						//"-s","1920x1080",
-// 						"-an",
-// 						//"-profile:v","baseline",//baseline
-// 						//"-vf","drawtext='fontfile=/home/pi/ffmpeg/freefont/FreeSans.ttf:text=%{localtime\}':fontsize=50:fontcolor=yellow@1:box=1:boxcolor=red@0.9:x=(w-tw)/2:y=10",
-// 						"-loglevel","error",
-// 						"-stats",
-// 						"-tune","zerolatency",
-// 						"-f","h264",
-// 						//"-reset_timestamps", "1",
-// 						//"-movflags","isml+empty_moov+faststart",//+faststart//"frag_keyframe+empty_moov",
-// 						//"-fflags","nobuffer",
-// 						//"-frag_duration","5",
-// 						"-y",
-// 						//"cam_video.mp4"
-// 						"-"
-// 						])
-    var proc = spawn('raspivid', [//work!
-    					'-t', '0',
-    					'-o', '-',
-    					"-n",
-    					'-w', 640,
-    					'-h', 360,
-    					'-fps', 30,
-    					'-pf', "baseline"//'baseline'
-    					]);
+var proc=spawn("ffmpeg",[
+	"-s","512x288",
+	"-re",
+	"-framerate","30",
+	//"-pix_fmt","yuv420p",//"yuv420p",//yuyv422 
+	"-i","/dev/video0",
+	// "-c:v","h264_mmal",
+	// "-i","/home/pi/360.mp4",
+	"-c:v","libx264",
+	"-b:v","1M",
+	//"-s","1920x1080",
+	"-an",
+	"-profile:v","baseline",//baseline
+	//"-vf","drawtext='fontfile=/home/pi/ffmpeg/freefont/FreeSans.ttf:text=%{localtime\}':fontsize=50:fontcolor=yellow@1:box=1:boxcolor=red@0.9:x=(w-tw)/2:y=10",
+	"-loglevel","error",
+	"-stats",
+	"-tune","zerolatency",
+	"-f","h264",
+	"-pix_fmt","yuv420p",
+	"-preset","ultrafast",
+	//"-reset_timestamps", "1",
+	//"-movflags","isml+empty_moov+faststart",//+faststart//"frag_keyframe+empty_moov",
+	//"-fflags","nobuffer",
+	//"-frag_duration","5",
+	"-y",
+	//"cam_video.mp4"
+	"-"
+	])
+    // var proc = spawn('raspivid', [//work!
+    // 					'-t', '0',
+    // 					'-o', '-',
+    // 					"-n",
+    // 					'-w', 640,
+    // 					'-h', 360,
+    // 					'-fps', 30,
+    // 					'-pf', "baseline"//'baseline'
+    // 					]);
 
 	var rawstream=proc.stdout.pipe(new Split(NALseparator))
 	
